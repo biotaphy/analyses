cc67f144a22ba09dfb7a620c9cb1b2b05fb4d0ed
CJ Grady <cjgrady@gmail.com>
2018-08-24 15:11:46 -0400

    PEP 8 updates

--- tests/test_helpers/test_data_readers.py
+++ tests/test_helpers/test_data_readers.py
@@ -6,10 +6,9 @@
 import pytest
 from StringIO import StringIO
 
-from ancestral_reconstruction.helpers.data_readers import (AlignmentIOError, 
-                           read_csv_alignment_flo, read_json_alignment_flo,
-                           read_phylip_alignment_flo, read_table_alignment_flo)
+import ancestral_reconstruction.helpers.data_readers as dr
 from ancestral_reconstruction.helpers.sequence import Sequence
+
 
 # .............................................................................
 class Test_create_sequence_list_from_dict(object):
@@ -19,15 +18,16 @@ class Test_create_sequence_list_from_dict(object):
     # .....................................
     def test_invalid_dict(self):
         pass
-   
+
     # .....................................
     def test_valid_dict(self):
         pass
 
+
 # .............................................................................
 class Test_read_csv_alignment_flo(object):
     """
-    @summary: Tests the read_csv_alignment_flo method
+    @summary: Tests the dr.read_csv_alignment_flo method
     """
     # .....................................
     def test_file_invalid(self, data_files):
@@ -38,9 +38,9 @@ class Test_read_csv_alignment_flo(object):
         invalid_csv_alignments = data_files.get_alignments('csv', False)
         for fn in invalid_csv_alignments:
             with open(fn) as in_csv:
-                with pytest.raises(AlignmentIOError):
-                   read_csv_alignment_flo(in_csv)
-   
+                with pytest.raises(dr.AlignmentIOError):
+                    dr.read_csv_alignment_flo(in_csv)
+
     # .....................................
     def test_file_valid(self, data_files):
         """
@@ -51,7 +51,7 @@ class Test_read_csv_alignment_flo(object):
         for fn in valid_csv_alignments:
             with open(fn) as in_csv:
                 try:
-                    sequence_list, headers = read_csv_alignment_flo(in_csv)
+                    sequence_list, headers = dr.read_csv_alignment_flo(in_csv)
                     assert len(headers) > 0
                     assert len(sequence_list) > 0
                     for i in sequence_list:
@@ -61,11 +61,11 @@ class Test_read_csv_alignment_flo(object):
                 except Exception, e:
                     print 'Raised exception:', str(e)
                     assert False
-   
+
     # .....................................
     def test_stringio_invalid(self, data_files):
         """
-        @summary: Tests that the invalid alignment files fail properly when 
+        @summary: Tests that the invalid alignment files fail properly when
                    loaded into StringIO objects
         @param data_files: A pytest fixture defined in conftest.py
         """
@@ -75,13 +75,13 @@ class Test_read_csv_alignment_flo(object):
                 csv_stringio = StringIO()
                 csv_stringio.write(in_csv.read())
                 csv_stringio.seek(0)
-                with pytest.raises(AlignmentIOError):
-                    read_csv_alignment_flo(csv_stringio)
-   
+                with pytest.raises(dr.AlignmentIOError):
+                    dr.read_csv_alignment_flo(csv_stringio)
+
     # .....................................
     def test_stringio_valid(self, data_files):
         """
-        @summary: Tests that the valid alignment files do not fail when loaded 
+        @summary: Tests that the valid alignment files do not fail when loaded
                      into StringIO objects
         @param data_files: A pytest fixture defined in conftest.py
         """
@@ -92,7 +92,8 @@ class Test_read_csv_alignment_flo(object):
                 csv_stringio.write(in_csv.read())
                 csv_stringio.seek(0)
                 try:
-                    sequence_list, headers = read_csv_alignment_flo(csv_stringio)
+                    sequence_list, headers = dr.read_csv_alignment_flo(
+                                                                csv_stringio)
                     assert len(headers) > 0
                     assert len(sequence_list) > 0
                     for i in sequence_list:
@@ -103,10 +104,11 @@ class Test_read_csv_alignment_flo(object):
                     print 'Raised exception:', str(e)
                     assert False
 
+
 # .............................................................................
 class Test_read_json_alignment_flo(object):
     """
-    @summary: Tests the read_json_alignment_flo method
+    @summary: Tests the dr.read_json_alignment_flo method
     """
     # .....................................
     def test_file_invalid(self, data_files):
@@ -117,25 +119,26 @@ class Test_read_json_alignment_flo(object):
         invalid_json_alignments = data_files.get_alignments('json', False)
         for fn in invalid_json_alignments:
             with open(fn) as in_json:
-                with pytest.raises(AlignmentIOError):
-                    read_json_alignment_flo(in_json)
-   
+                with pytest.raises(dr.AlignmentIOError):
+                    dr.read_json_alignment_flo(in_json)
+
     # .....................................
     def test_file_valid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_invalid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_valid(self, data_files):
         pass
 
+
 # .............................................................................
 class Test_read_phylip_alignment_flo(object):
     """
-    @summary: Tests the read_phylip_alignment_flo method
+    @summary: Tests the dr.read_phylip_alignment_flo method
     """
     # .....................................
     def test_file_invalid(self, data_files):
@@ -146,9 +149,9 @@ class Test_read_phylip_alignment_flo(object):
         invalid_phylip_alignments = data_files.get_alignments('phylip', False)
         for fn in invalid_phylip_alignments:
             with open(fn) as in_phylip:
-                with pytest.raises(AlignmentIOError):
-                    read_phylip_alignment_flo(in_phylip)
-   
+                with pytest.raises(dr.AlignmentIOError):
+                    dr.read_phylip_alignment_flo(in_phylip)
+
     # .....................................
     def test_file_valid(self, data_files):
         pass
@@ -156,10 +159,11 @@ class Test_read_phylip_alignment_flo(object):
     # .....................................
     def test_stringio_invalid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_valid(self, data_files):
         pass
+
 
 # .............................................................................
 class Test_read_phylip_continuous_alignment_flo(object):
@@ -169,18 +173,19 @@ class Test_read_phylip_continuous_alignment_flo(object):
     # .....................................
     def test_file_invalid(self, data_files):
         pass
-   
+
     # .....................................
     def test_file_valid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_invalid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_valid(self, data_files):
         pass
+
 
 # .............................................................................
 class Test_read_table_continuous_alignment_flo(object):
@@ -196,17 +201,17 @@ class Test_read_table_continuous_alignment_flo(object):
         invalid_table_alignments = data_files.get_alignments('table', False)
         for fn in invalid_table_alignments:
             with open(fn) as in_table:
-                with pytest.raises(AlignmentIOError):
-                    read_table_alignment_flo(in_table)
-   
+                with pytest.raises(dr.AlignmentIOError):
+                    dr.read_table_alignment_flo(in_table)
+
     # .....................................
     def test_file_valid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_invalid(self, data_files):
         pass
-   
+
     # .....................................
     def test_stringio_valid(self, data_files):
         pass
