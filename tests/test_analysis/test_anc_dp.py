f6c127cd6e5daedca0a47775f4a35dde41e964e3
CJ Grady <cjgrady@gmail.com>
2018-08-27 11:42:33 -0400

    Test invalid packages

--- tests/test_analysis/test_anc_dp.py
+++ tests/test_analysis/test_anc_dp.py
@@ -12,7 +12,62 @@ import ancestral_reconstruction.helpers.data_readers as data_readers
 
 
 # .............................................................................
-def test_calculate_continuous_ancestral_states(data_files):
+def test_calculate_continuous_ancestral_states_invalid(data_files):
+    """
+    @summary: Tests the calculate_continusous_ancestral_states method with
+                invalid data
+    @note: This test will need to evolve as the output format changes.  It
+            will probably be better to return a data structure with various
+            values for each node rather than assigning the value to the node
+            label
+    """
+    # Get the data files
+    packages = data_files.get_packages(False)
+    assert len(packages) > 0
+    for tree_filename, alignment_filename, results_filename in packages:
+        # Process the tree file
+        _, tree_ext = os.path.splitext(tree_filename)
+        if tree_ext == '.nex':
+            tree_schema = 'nexus'
+        elif tree_ext == '.xml':
+            tree_schema = 'nexml'
+        elif tree_ext == '.tre':
+            tree_schema = 'newick'
+        else:
+            raise Exception(
+                'Cannot handle tree with extension: {}'.format(tree_ext))
+        tree = dendropy.Tree.get(path=tree_filename, schema=tree_schema)
+
+        # Process the alignment file
+        _, align_ext = os.path.splitext(alignment_filename)
+        if align_ext == '.csv':
+            with open(alignment_filename) as align_file:
+                sequences, headers = data_readers.read_csv_alignment_flo(
+                                                                align_file)
+        elif align_ext == '.json':
+            with open(alignment_filename) as align_file:
+                sequences, headers = data_readers.read_json_alignment_flo(
+                                                                align_file)
+        elif align_ext == '.phylip':
+            with open(alignment_filename) as align_file:
+                sequences = data_readers.read_phylip_alignment_flo(
+                                                                align_file)
+        elif align_ext == '.tbl':
+            with open(alignment_filename) as align_file:
+                sequences = data_readers.read_table_alignment_flo(
+                                                                align_file)
+        else:
+            raise Exception(
+                'Cannot handle alignments with extension: {}'.format(
+                    align_ext))
+
+        # Run analysis
+        with pytest.raises(Exception):
+            anc_dp.calculate_continuous_ancestral_states(tree, sequences)
+
+
+# .............................................................................
+def test_calculate_continuous_ancestral_states_valid(data_files):
     """
     @summary: Tests the calculate_continusous_ancestral_states method
     @note: This test will need to evolve as the output format changes.  It
@@ -21,7 +76,7 @@ def test_calculate_continuous_ancestral_states(data_files):
             label
     """
     # Get the data files
-    packages = data_files.get_packages()
+    packages = data_files.get_packages(True)
     assert len(packages) > 0
     for tree_filename, alignment_filename, results_filename in packages:
         # Process the tree file
