aa3a6e9e28fa46da9ef97bc58ecdeb6ed785ec48
CJ Grady <cjgrady@gmail.com>
2018-12-12 12:55:47 -0500

    Added methods for formatting outputs

--- bin/ancestral_distribution.py
+++ bin/ancestral_distribution.py
@@ -9,11 +9,10 @@ Todo:
 import argparse
 import os
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-from ancestral_reconstruction.helpers import data_readers
 from ancestral_reconstruction.analysis import anc_dp
+import ancestral_reconstruction.tools.annotators as annotators
+import ancestral_reconstruction.tools.plots as tree_plots
+from ancestral_reconstruction.helpers import data_readers
 from ancestral_reconstruction.lm_objects.tree import TreeWrapper
 
 DESCRIPTION = """\
@@ -25,33 +24,50 @@ distributions at the tips of the tree"""
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESCRIPTION)
 
-    parser.add_argument('in_tree_filename', type=str, 
-                        help='Path to the tree file')
-    parser.add_argument('in_tree_schema', type=str, 
-                        help='The format of the tree', 
-                        choices=['newick', 'nexml', 'nexus'])
+    parser.add_argument(
+        'in_tree_filename', type=str, help='Path to the tree file')
+    parser.add_argument(
+        'in_tree_schema', type=str, help='The format of the tree',
+        choices=['newick', 'nexml', 'nexus'])
    
-    parser.add_argument('data_filename', type=str, 
-                        help='Path to file with character state data')
-    parser.add_argument('data_format', type=str, 
-                        help='The format of the character data', 
-                        choices=['csv', 'json', 'phylip', 'table'])
-    # out directory
-    parser.add_argument('-o', '--output_directory', type=str, 
-                        help='If provided, write the rates and plots here')
+    parser.add_argument(
+        'data_filename', type=str,
+        help='Path to file with character state data')
+    parser.add_argument(
+        'data_format', type=str, help='The format of the character data',
+        choices=['csv', 'json', 'phylip', 'table'])
 
-    # print tree
-    # parser.add_argument('--print_tree', action='store_true', required=False,
-    #                     help='Show the tree (requires ete3)')
+    # Outputs
+    # Annotated tree or trees
+    # Plots
+    # Matrix csv
+    parser.add_argument(
+        'out_tree_filename', type=str,
+        help='Path to write the resulting annotated tree')
+    parser.add_argument(
+        'out_tree_schema', type=str,
+        help='The format to use when writing the tree',
+        choices=['newick', 'nexml', 'nexus'])
+    parser.add_argument(
+        '-l', '--annotate_labels', type=str,
+        help='If provided, annotate the tree labels with this data column')
+    parser.add_argument(
+        '-p', '--plot_directory', type=str,
+        help='If provided, write distribution plots to this directory')
+    parser.add_argument(
+        '-c', '--out_csv_filename', type=str,
+        help='If provided, write the output character matrix CSV ',
+             'to this file location')
+    
     args = parser.parse_args()
 
     # Check that input files exist
     if not os.path.exists(args.in_tree_filename):
-       raise IOError('Input tree {} does not exist'.format(
-                                                       args.in_tree_filename))
+       raise IOError(
+           'Input tree {} does not exist'.format(args.in_tree_filename))
     if not os.path.exists(args.data_filename):
-       raise IOError('Input data file {} does not exist'.format(
-                                                      args.data_filename))
+       raise IOError(
+           'Input data file {} does not exist'.format(args.data_filename))
 
     # Read the tree
     tree = TreeWrapper.get(path=args.in_tree_filename, 
@@ -75,52 +91,44 @@ if __name__ == '__main__':
     else:
         raise Exception, 'Unknown data format: {}'.format(args.data_format)
 
+    # Get the label annotation column, or None
+    label_column = None
+    if args.annotate_labels is not None:
+        try:
+            # Try looking for the string
+            label_column = headers.index(args.annotate_labels)
+        except:
+            try:
+                # Treat it as an integer
+                label_column = int(args.annotate_labels)
+            except:
+                raise Exception(
+                    'Could not find column to use for labels.  ',
+                    'Check the name to make sure it matches or use column'
+                    ' index.')
+
     # Get character matrix
     char_mtx = data_readers.get_character_matrix_from_sequences_list(
         sequences, var_headers=headers)
     # Run analysis
     tree, results = anc_dp.calculate_ancestral_distributions(tree, char_mtx)
 
-    # Write outputs
-    if args.output_directory is not None:
-        # Write plots
-        node_names = results.get_row_headers()
-        num_cats = char_mtx.data.shape[1]
-        low = 0
-        high = num_cats
-        
-        x_grid = np.linspace(low, high, num_cats)
-        for i in range(len(node_names)):
-            plt.figure(figsize=(6, 4))
-            plt.plot(x_grid, results.data[i, :, 0])
-            plt.fill_between(x_grid, 0, results.data[i, :, 0], alpha=0.05)
-            if np.any(results.data[i, :, 1] > 0.0):
-                high_vals = results.data[i, :, 0] + results.data[i, :, 1]
-                low_vals = results.data[i, :, 0] - results.data[i, :, 1]
-                plt.plot(x_grid, high_vals, '--', alpha=0.55,)
-                plt.plot(x_grid, low_vals, '--', alpha=0.55,)
-            plt.grid(True)
-            plt.savefig(os.path.join(args.output_directory, 
-                                     '{}.png'.format(node_names[i])))
-            plt.close()
+    # Should we annotate the tree labels?
+    if label_column is not None:
+        annotators.annotate_tree_with_label(
+            tree, results, label_column=label_column)
+    else:
+        # Annotate tree
+        annotators.add_all_annotations(tree, results, update=True)
 
-        # Add rates to output
-        # TODO: Calculate rates
-        # plt.plot(x_grid, rates)
-        # plt.ylabel('estimated rate')
-        # plt.xlabel('state space')
-        # plt.savefig(os.path.join(args.output_directory, 'rate_estimate.png'))
-
-        # Write labeled tree
-        tree.write(path=os.path.join(args.output_directory, 
-                                     'labeled_tree.tre'), schema='newick')
-
-        # Write results matrix (as csv)
-        distributions_filename = os.path.join(args.output_directory, 
-                                              'distributions.csv')
-        with open(distributions_filename, 'w') as distributions_file:
-            results.write_csv(distributions_file)
+    # Write the tree
+    tree.write(path=args.out_tree_filename, schema=args.out_tree_schema)
     
-    # If we should print the tree, do it
-    # if args.print_tree:
-    #     print_tree_to_file(tree, args.output_directory)
+    # CSV
+    if args.out_csv_filename is not None:
+        with open(args.out_csv_filename, 'w') as out_csv_f:
+            results.write_csv(out_csv_f)
+    # Plots
+    if args.plot_directory is not None:
+        tree_plots.create_distribution_plots(
+            tree, results, args.plot_directory)
