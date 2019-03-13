920045d1bc57bc1228a4684bbc04deebca33e5a9
CJ Grady <cjgrady@gmail.com>
2019-03-13 17:41:16 -0500

    Adds test for script executable

---
+++ tests/test_bin/test_ancestral_distribution.py
@@ -0,0 +1,133 @@
+"""This module tests the ancestral_distribution executable script
+"""
+import numpy as np
+import os
+import pytest
+import subprocess
+
+
+# .............................................................................
+class Test_ancestral_distribution(object):
+    """Test ancestral distribution reconstruction
+    """
+    script_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        '../../bin/ancestral_distribution.py')
+
+    # .....................................
+    def test_package_valid(self, data_files, tmpdir):
+        """Tests the calculate_ancestral_distributions method.
+
+        Args:
+            data_files (pytest.fixture): A pytest fixture defined in
+                conftest.py for retrieving test data.
+
+        Raises:
+            IOError: When the tree or alignment cannot be loaded for the
+                specified file extension.
+            Exception: When a specified successful result value cannot be
+                found.
+        """
+        # Get the data files
+        packages = data_files.get_ancestral_distribution_packages(True)
+        assert len(packages) > 0
+        for tree_filename, alignment_filename, results_filename in packages:
+
+            # Process the tree file
+            _, tree_ext = os.path.splitext(tree_filename)
+            if tree_ext == '.nex':
+                tree_schema = 'nexus'
+            elif tree_ext == '.xml':
+                tree_schema = 'nexml'
+            elif tree_ext == '.tre':
+                tree_schema = 'newick'
+            else:
+                raise IOError(
+                    'Cannot handle tree with extension: {}'.format(tree_ext))
+
+            # Process alignment file
+            _, align_ext = os.path.splitext(alignment_filename)
+            if align_ext == '.csv':
+                alignment_format = 'csv'
+            elif align_ext == '.json':
+                alignment_format = 'json'
+            elif align_ext == '.phylip':
+                alignment_format = 'phylip'
+            elif align_ext == '.tbl':
+                alignment_format = 'table'
+            else:
+                raise IOError(
+                    'Cannot handle alignments with extension: {}'.format(
+                        align_ext))
+
+            csv_filename = os.path.join(tmpdir.dirname, 'test_out.csv')
+            out_tree_filename = os.path.join(tmpdir.dirname, 'test_out.nex')
+            cmd = '{} -c {} {} {} {} {} {} nexus'.format(
+                self.script_path, csv_filename, tree_filename,
+                tree_schema, alignment_filename, alignment_format,
+                out_tree_filename)
+
+            # Call process
+            res = subprocess.check_call(cmd, shell=True)
+
+            assert res == 0
+
+            # Load output matrix
+            out_ml_results = []
+            out_std_err_results = []
+            h = None
+            with open(csv_filename) as output_file:
+                for line in output_file:
+                    if h is None:
+                        # Get headers
+                        h = line.strip().split(',')[1:]
+                    else:
+                        # Add result (without label) to appropriate list
+                        parts = line.strip().split(',')
+                        layer = parts[1].lower()
+                        values = np.array(
+                            [float(i) for i in parts[2:]], dtype=np.float)
+                        if layer == 'maximum_likelihood':
+                            out_ml_results.append(values)
+                        else:
+                            out_std_err_results.append(values)
+            assert(len(out_ml_results) == len(out_std_err_results))
+
+            # Load desired result matrix
+            test_ml_results = []
+            test_std_err_results = []
+            h = None
+            with open(csv_filename) as output_file:
+                for line in output_file:
+                    if h is None:
+                        # Get headers
+                        h = line.strip().split(',')[1:]
+                    else:
+                        # Add result (without label) to appropriate list
+                        parts = line.strip().split(',')
+                        layer = parts[1].lower()
+                        values = np.array(
+                            [float(i) for i in parts[2:]], dtype=np.float)
+                        if layer == 'maximum_likelihood':
+                            test_ml_results.append(values)
+                        else:
+                            test_std_err_results.append(values)
+            assert(len(test_ml_results) == len(test_std_err_results))
+
+            # Compare results
+            for i in range(len(out_ml_results)):
+                ml_row = out_ml_results[i]
+                std_err_row = out_std_err_results[i]
+
+                for i in range(len(test_ml_results)):
+                    if np.all(np.isclose(ml_row, test_ml_results[i])) and \
+                            np.all(np.isclose(
+                                std_err_row, test_std_err_results[i])):
+                        found = True
+                        test_ml_results.pop(i)
+                        test_std_err_results.pop(i)
+                        break
+                if not found:
+                    raise Exception(
+                        'Could not find {}, {} in results'.format(
+                            ml_row, std_err_row))
